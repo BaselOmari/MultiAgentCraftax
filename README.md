f4c4d13eb569a0f9f667e7025ac439b61f1ffebf
Bassel Al Omari <b2alomar@uwaterloo.ca>
2025-03-03 15:40:26 -0500

    multi-agent initial commit

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# MultiAgentCraftax
-(Crafter + NetHack) but Multi-Agent in JAX
+<p align="center">
+ <img width="80%" src="https://raw.githubusercontent.com/MichaelTMatthews/Craftax/main/images/logo.png" />
+</p>
+
+# ⛏️ Multi-Agent Craftax
+Multi-Agent Craftax is an RL environment written entirely in <a href="https://github.com/google/jax">JAX</a>.  It is an extension of the recent hit, Craftax which reimplements and significantly extends the game mechanics of <a href="https://github.com/danijar/crafter">Crafter</a>, taking inspiration from roguelike games such as <a href="https://github.com/facebookresearch/nle">NetHack</a>. All the flair of Craftax, but now for the multi-agent setting!
+
+Get ready to run 2 billion environment interactions in less than an hour!
+
+Craftax conforms to the <a href="https://github.com/FLAIROx/JaxMARL/tree/main">JaxMARL</a> interface, allowing easy integration with existing JAX-based multi-agent RL baselines.
+
+<p align="middle">
+  <img src="https://raw.githubusercontent.com/MichaelTMatthews/Craftax/main/images/archery.gif" width="200" />
+  <img src="https://raw.githubusercontent.com/MichaelTMatthews/Craftax/main/images/building.gif" width="200" /> 
+  <img src="https://raw.githubusercontent.com/MichaelTMatthews/Craftax/main/images/dungeon_crawling.gif" width="200" />
+</p>
+<p align="middle">
+  <img src="https://raw.githubusercontent.com/MichaelTMatthews/Craftax/main/images/farming.gif" width="200" />
+  <img src="https://raw.githubusercontent.com/MichaelTMatthews/Craftax/main/images/magic.gif" width="200" /> 
+  <img src="https://raw.githubusercontent.com/MichaelTMatthews/Craftax/main/images/mining.gif" width="200" />
+</p>
+
+# 📜 Basic Usage
+Craftax conforms to the gymnax interface:
+```python
+rng = jax.random.PRNGKey(0)
+rng, _rng = jax.random.split(rng)
+rngs = jax.random.split(_rng, 3)
+
+# Create environment
+env = make_craftax_env_from_name("Craftax-Symbolic-v1", auto_reset=True)
+env_params = env.default_params
+
+# Get an initial state and observation
+obs, state = env.reset(rngs[0], env_params)
+
+# Pick random action
+action = env.action_space(env_params).sample(rngs[1])
+
+# Step environment
+obs, state, reward, done, info = env.step(rngs[2], state, action, env_params)
+```
+```
+import jax
+from craftax.craftax_env import make_craftax_env_from_name
+
+key = jax.random.PRNGKey(0)
+key, key_reset, key_act, key_step = jax.random.split(key, 4)
+
+# Initialise environment.
+env = make_craftax_env_from_name('Craftax-MARL-Symbolic-v1')
+
+# Reset the environment.
+obs, state = env.reset(key_reset)
+
+# Sample random actions.
+key_act = jax.random.split(key_act, env.num_agents)
+actions = {agent: env.action_space(agent).sample(key_act[i]) for i, agent in enumerate(env.agents)}
+
+# Perform the step transition.
+obs, state, reward, done, infos = env.step(key_step, state, actions)
+```
+
+## GPU-Enabled JAX
+By default, both of the above methods will install JAX on the CPU.  If you want to run JAX on a GPU/TPU, you'll need to install the correct wheel for your system from <a href="https://github.com/google/jax?tab=readme-ov-file#installation">JAX</a>.
+For NVIDIA GPU the command is:
+```
+pip install -U "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+```
+
---
+++ craftax/craftax_marl/envs/craftax_pixels_env.py
@@ -0,0 +1,113 @@
+import chex
+import jax
+from functools import partial
+from jax import lax
+from jaxmarl.environments import spaces
+from jaxmarl.environments.multi_agent_env import MultiAgentEnv
+from typing import Dict, Tuple
+
+from craftax_marl.constants import *
+from craftax_marl.craftax_state import EnvState, EnvParams, StaticEnvParams
+from craftax_marl.envs.common import compute_score
+from craftax_marl.game_logic import craftax_step
+from craftax_marl.renderer.renderer_pixels import render_craftax_pixels
+from craftax_marl.util.game_logic_utils import has_beaten_boss
+from craftax_marl.world_gen.world_gen import generate_world
+
+
+class CraftaxMARLPixelsEnv(MultiAgentEnv):
+    def __init__(self, num_agents: int = 3):
+        self.num_agents = num_agents
+        self.static_env_params = CraftaxMARLPixelsEnv.default_static_params()
+
+        self.agents = [
+            f"agent_{i}" for i in range(self.static_env_params.player_count)
+        ]
+        self.action_spaces = {name: self.action_shape() for name in self.agents}
+        self.observation_spaces = {name: self.observation_shape() for name in self.agents}
+
+        self.player_specific_textures = load_player_specific_textures(
+            TEXTURES[BLOCK_PIXEL_SIZE_HUMAN],
+            self.static_env_params.player_count
+        )
+
+    @partial(jax.jit, static_argnums=(0,))
+    def reset(self, key: chex.PRNGKey) -> Tuple[Dict[str, chex.Array], EnvState]:
+        state = generate_world(key, self.default_params, self.static_env_params)
+        return self.get_obs(state), state
+    
+    @partial(jax.jit, static_argnums=(0,))
+    def step_env(
+        self, key: chex.PRNGKey, state: EnvState, actions: Dict[str, chex.Array]
+    ) -> Tuple[Dict[str, chex.Array], EnvState, Dict[str, float], Dict[str, bool], Dict]:
+        actions = jnp.array(list(actions.values()))
+        state, reward = craftax_step(key, state, actions, self.default_params, self.static_env_params)
+
+        obs = self.get_obs(state)
+        done = self.is_terminal(state, self.default_params)
+        info = compute_score(state, done)
+        info["discount"] = self.discount(state, self.default_params)
+
+        agent_rewards = {n: r for n,r in zip(self.agents, reward)}
+
+        agent_done = {n: done for n in self.agents}
+        agent_done["__all__"] = done
+
+        return (
+            obs,
+            lax.stop_gradient(state),
+            agent_rewards,
+            agent_done,
+            info,
+        )
+
+    @partial(jax.jit, static_argnums=(0,))
+    def get_obs(self, state: EnvState) -> chex.Array:
+        pixels = lax.stop_gradient(
+                render_craftax_pixels(
+                state, 
+                BLOCK_PIXEL_SIZE_HUMAN, 
+                self.static_env_params,
+                self.player_specific_textures
+            ) / 255.0
+        )
+        obs = {n:o for n,o in zip(self.agents, pixels)}
+        return obs
+    
+    @property
+    def default_params(self) -> EnvParams:
+        return EnvParams()
+    
+    @staticmethod
+    def default_static_params() -> StaticEnvParams:
+        return StaticEnvParams()
+    
+    def action_shape(self) -> spaces.Discrete:
+        return spaces.Discrete(len(Action) + (self.static_env_params.player_count - 1))
+        
+    def observation_shape(self) -> spaces.Box:
+        map_height = OBS_DIM[0]
+        inventory_height = INVENTORY_OBS_HEIGHT
+        teammate_dashboard_height = (self.static_env_params.player_count+1)//2
+        return spaces.Box(
+            0.0,
+            1.0,
+            (
+                OBS_DIM[1] * BLOCK_PIXEL_SIZE_HUMAN,
+                (map_height + inventory_height + teammate_dashboard_height) * BLOCK_PIXEL_SIZE_HUMAN,
+                3,
+            ),
+            dtype=jnp.float32,
+        )
+    
+    def is_terminal(self, state: EnvState, params: EnvParams) -> bool:
+        done_steps = state.timestep >= params.max_timesteps
+        is_dead = jnp.logical_not(state.player_alive).all()
+        defeated_boss = has_beaten_boss(state, self.static_env_params)
+        is_terminal = jnp.logical_or(is_dead, done_steps)
+        is_terminal = jnp.logical_or(is_terminal, defeated_boss)
+        return is_terminal
+    
+    def discount(self, state, params) -> float:
+        """Return a discount of zero if the episode has terminated."""
+        return jax.lax.select(self.is_terminal(state, params), 0.0, 1.0)
